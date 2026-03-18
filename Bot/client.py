572b9b7a6b3b524d7246e5d93146cb76bae1f8c5
adt-thp1905 <aadityathapliyal03@gmail.com>
2026-03-18 17:32:58 +0530

    Update client.py

--- Bot/client.py
+++ Bot/client.py
@@ -1,1 +1,16 @@
+import os
+from binance.client import Client
+from dotenv import load_dotenv
 
+load_dotenv()
+
+class BinanceFuturesClient:
+    def __init__(self):
+        self.client = Client(
+            os.getenv("BINANCE_API_KEY"),
+            os.getenv("BINANCE_API_SECRET")
+        )
+        self.client.FUTURES_URL = "https://testnet.binancefuture.com/fapi"
+
+    def place_order(self, **params):
+        return self.client.futures_create_order(**params)
